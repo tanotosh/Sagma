0ea630575cf0f582a40788e725659046a133984e
tanotosh <s.tanoto@mail.utoronto.ca>
2024-11-18 18:17:30 -0500

    update function names

--- src/main/java/use_case/Search.java
+++ src/main/java/use_case/Search.java
@@ -13,15 +13,15 @@ public class Search {
         this.cuisineToFood = cuisineToFood; //not correct, need to figure out how to read database to compile all foods
     }
 
-    public Set<String> getcuisines() {
+    public Set<String> getCuisines() {
         return cuisineToFood.keySet();
     }
 
-    public List<Food> getfilteredfoods(String cuisine){ //takes from choice in search page
+    public List<Food> getFilteredFoods(CommonUser user, String cuisine){ //takes from choice in search page
         List<Food> filteredFoods = new List<Food>;
         filtered_cuisine = new List<Food>;
         for (String food : cuisineToFood.get(cuisine) { // need to add private instances dietary restrictionsin CommonUser
-            for (String restriction: CommonUser.dietaryRestrictions){
+            for (String restriction: user.getDietaryRestrictions()){
                 if restriction in food.dietaryRestrictions{
                     filteredFoods.add(food);
                 }
