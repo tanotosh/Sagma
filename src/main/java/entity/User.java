731f3ee4c0a8d777cb5fd065ffc76f58d4459d85
MaxNguyen5040 <MaxNguyen5040>
2024-11-25 18:36:00 -0500

    fixed User attributes and added getters or setters.

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,16 +1,28 @@
 package entity;
 
+import java.io.File;
+import java.util.*;
+
 /**
- * The representation of a user in our program. We combined Common User and User into this.
+ * A simple implementation of the User interface.
  */
 public class User {
-
     private final String name;
     private final String password;
+    private Food currentFood;
+    private List<Food> foodUploads; // past foods which there are no more servings of
+    private float rating; // the average of all the ratings of all the foods in foodUploads AND currentFood (1). Has to be updated every time a new rating is updated, or gets re calculated every time it’s displayed. Probably better to update it each time something gets uploaded
+    private int ratingsCount; // number of ratings in current rating tally
+    private String dietaryRestrictions;
 
     public User(String name, String password) {
         this.name = name;
         this.password = password;
+        this.currentFood = null;
+        this.foodUploads = new ArrayList<Food>();
+        this.rating = 0;
+        this.ratingsCount = 0;
+        this.dietaryRestrictions = null;
     }
 
     public String getName() {
@@ -21,4 +33,56 @@ public class User {
         return password;
     }
 
+    public void uploadFood(String name, int quantity, String ingredients, String dietaryRestrictions,
+                           File image, String category){
+        this.currentFood = new Food(name, quantity, ingredients, dietaryRestrictions, image, category);
+    }
+
+    public void swipeRight(Food food) {
+        food.addSwipedYes(this);
+    }
+
+    public void swipeLeft(Food food) {
+        food.addSwipedNo(this);
+    }
+
+    public Food getCurrentFood() {
+        return currentFood;
+    }
+
+    public void setCurrentFood(Food currentFood) {
+        this.currentFood = currentFood;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public int getRatingsCount() {
+        return ratingsCount;
+    }
+
+    public void setRatingsCount(int ratingsCount) {
+        this.ratingsCount = ratingsCount;
+    }
+
+    public List<Food> getFoodUploads() {
+        return foodUploads;
+    }
+
+    public void setFoodUploads(List<Food> foodUploads) {
+        this.foodUploads = foodUploads;
+    }
+
+    public String getDietaryRestrictions() {
+        return dietaryRestrictions;
+    }
+
+    public void setDietaryRestrictions(String dietaryRestrictions) {
+        this.dietaryRestrictions = dietaryRestrictions;
+    }
 }
